5dff1f08561576710577f4791a4c67abf64b4449
kodehost <ahmadpoor.reza@gmail.com>
2021-12-28 10:44:51 +0330

    create main.go file

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi! Dont Go")
+
+	pace := 0
+	for i := 1; i < 10; i++ {
+		pace += i
+		fmt.Println("pace address:", &pace, "  /  Value:", pace)
+
+	}
+
+}

64137062b9dcb84e2821019b62aa6ae6e96b672c
Reza <57559062+kodehost@users.noreply.github.com>
2024-06-18 07:05:59 +0330

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("Hi! Dont Go")
 
-	pace := 0
+	pace := 10
 	for i := 1; i < 10; i++ {
 		pace += i
 		fmt.Println("pace address:", &pace, "  /  Value:", pace)
